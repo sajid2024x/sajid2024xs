320734fa015fa84fb54a90715c767b93d23e2519
sajid2024x <mdsajinurrahman.sajid.2024@gmail.com>
2026-02-24 16:22:19 +0600

    Update collector.py

--- collector.py
+++ collector.py
@@ -4,6 +4,7 @@ import os
 from watcher import WATCHLIST
 
 client = tweepy.Client(
+    bearer_token=os.getenv("X_BEARER_TOKEN"),
     consumer_key=os.getenv("X_API_KEY"),
     consumer_secret=os.getenv("X_API_SECRET"),
     access_token=os.getenv("X_ACCESS_TOKEN"),
